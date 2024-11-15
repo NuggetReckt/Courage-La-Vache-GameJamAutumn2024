0601dcfccf9933bc8a5ce5c35b3410e470d440f4
NuggetReckt <corto.morrow@gmail.com>
2024-11-16 00:34:24 +0100

    fix: player name contains verif

--- scripts/generator.gd
+++ scripts/generator.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var bucket: Sprite2D = $Bucket
 @onready var empty_bucket: Sprite2D = $EmptyBucket
+@onready var area_2d: Area2D = $Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,13 +12,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var overlapping_areas = area_2d.get_overlapping_areas()
 	
+	for area: Area2D in overlapping_areas:
+		if (is_player(area)):
+			print("Lets goo")
 	pass
 	
-func toggle_bucket(delta: float) -> void:
+func toggle_bucket() -> void:
 	if (bucket.visible):
 		bucket.visible = false
 		empty_bucket.visible = true
 	else:
 		bucket.visible = true
 		empty_bucket.visible = false
+	
+func is_player(area: Area2D) -> bool:
+	return area.get_parent().name.contains("player")
