1f3438b8d4187f6ec7a3f32f26342324b331c90f
NuggetReckt <corto.morrow@gmail.com>
2024-11-15 22:51:05 +0100

    feat: bucket item empty/full toggler

---
+++ scripts/generator.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@onready var bucket: Sprite2D = $Bucket
+@onready var empty_bucket: Sprite2D = $EmptyBucket
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	bucket.visible = false
+	empty_bucket.visible = true
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func toggle_bucket(delta: float) -> void:
+	if (bucket.visible):
+		bucket.visible = false
+		empty_bucket.visible = true
+	else:
+		bucket.visible = true
+		empty_bucket.visible = false
