6d305acf1e61c9a99d590959683fccc1fc0606de
NuggetReckt <corto.morrow@gmail.com>
2024-11-16 03:24:38 +0100

    feat: Bucket multiple states
    feat: state toggler on button click

--- scripts/generator.gd
+++ scripts/generator.gd
@@ -1,14 +1,24 @@
 extends Node2D
 
-@onready var bucket: Sprite2D = $Bucket
 @onready var empty_bucket: Sprite2D = $EmptyBucket
+@onready var one_quarter_bucket: Sprite2D = $OneQuarterBucket
+@onready var three_quarters_bucket: Sprite2D = $ThreeQuartersBucket
+@onready var bucket: Sprite2D = $Bucket
 @onready var area_2d: Area2D = $Area2D
+
+var BUCKET_SPRITES = {
+	Main.BUCKET_STATE.EMPTY: empty_bucket,
+	Main.BUCKET_STATE.ONE_QUARTER: one_quarter_bucket,
+	Main.BUCKET_STATE.THREE_QUARTERS: three_quarters_bucket,
+	Main.BUCKET_STATE.FULL: bucket
+}
 
 var time_held: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	bucket.visible = false
+	for sprite: Sprite2D in BUCKET_SPRITES:
+		sprite.visible = false
 	empty_bucket.visible = true
 	pass # Replace with function body.
 
@@ -32,14 +42,19 @@ func _process(delta: float) -> void:
 	
 func toggle_bucket() -> void:
 	var fill_count: int = Main.get_bucket_fill_count()
+	var state: Main.BUCKET_STATE =  Main.get_bucket_state()
+	var new_state: Main.BUCKET_STATE
 	
-	if (bucket.visible):
-		bucket.visible = false
-		empty_bucket.visible = true
-	else:
-		bucket.visible = true
-		empty_bucket.visible = false
+	if (state >= Main.BUCKET_STATE.size()):
+		new_state = 0
 		Main.set_bucket_fill_count(fill_count + 1)
+	else:
+		new_state += 1
+	
+	BUCKET_SPRITES.get(state).visible = false
+	BUCKET_SPRITES.get(new_state).visible = true
+	
+	Main.set_bucket_state(state)
 
 func is_player(area: Area2D) -> bool:
 	return area.get_parent().name.contains("player")
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,10 +1,14 @@
 extends Node
 
+enum BUCKET_STATE {EMPTY, ONE_QUARTER, THREE_QUARTERS, FULL}
+
 var bucket_fill_count: int
+var bucket_current_state: BUCKET_STATE
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	bucket_fill_count = 0
+	bucket_current_state = BUCKET_STATE.EMPTY
 	pass # Replace with function body.
 	
 func _unhandled_input(event: InputEvent) -> void:
@@ -21,3 +25,9 @@ func set_bucket_fill_count(count: int) -> void:
 
 func get_bucket_fill_count() -> int:
 	return bucket_fill_count
+	
+func set_bucket_state(state: BUCKET_STATE) -> void:
+	bucket_current_state = state
+	
+func get_bucket_state() -> BUCKET_STATE:
+	return bucket_current_state
