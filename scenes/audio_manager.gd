b098dd5b0ade859f0f06d048dc8403b5352d64ba
NuggetReckt <corto.morrow@gmail.com>
2024-11-16 12:42:23 +0100

    feat: AudioManager for managing soundsfx & musics
    feat: Imported all sounds (ready to be used)

---
+++ scenes/audio_manager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+@onready var mooh_1_sfx: AudioStreamPlayer = $Mooh1SFX
+@onready var mooh_2_sfx: AudioStreamPlayer = $Mooh2SFX
+@onready var mooh_3_sfx: AudioStreamPlayer = $Mooh3SFX
+@onready var game_start_sfx: AudioStreamPlayer = $GameStartSFX
+@onready var game_end_sfx: AudioStreamPlayer = $GameEndSFX
+@onready var catch_attempt_1_sfx: AudioStreamPlayer = $CatchAttempt1SFX
+@onready var catch_attempt_2_sfx: AudioStreamPlayer = $CatchAttempt2SFX
+@onready var catch_1_sfx: AudioStreamPlayer = $Catch1SFX
+@onready var catch_2_sfx: AudioStreamPlayer = $Catch2SFX
+@onready var menu_theme: AudioStreamPlayer = $MenuTheme
+@onready var game_theme: AudioStreamPlayer = $GameTheme
