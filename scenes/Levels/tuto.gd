2c243a0b1ebd17b860c36b5f1ba7c74fad1799ee
Mecagon <justin.lepinepro@protonmail.com>
2024-11-16 12:30:34 +0100

    Tuto Transparence + Hunter tuto update

--- scenes/Levels/tuto.gd
+++ scenes/Levels/tuto.gd
@@ -3,12 +3,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	find_child("hunter").black_hole.modulate = Color(1,1,1,0.5)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	find_child("hunter").black_hole.modulate = Color(1,1,1,0.5)
 	if (Main.generator_charge_count >= 2):
 		if (randi()%2==1):
 			get_tree().change_scene_to_file("res://scenes/Lvl1.tscn")
