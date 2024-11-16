db275656a6eedb191c427c99577606d9a9742494
herbyvor <50072047+jeanfrancoisemil@users.noreply.github.com>
2024-11-16 15:46:31 +0100

    terrain mieux

--- scenes/Levels/tuto.gd
+++ scenes/Levels/tuto.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var night : bool = false
+var night : bool = true
 # Called when the node enters the scene tree for the first time.
 
 func _ready() -> void:
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 	find_child("hunter").black_hole.modulate = Color(1,1,1,0.5)
 	if (Main.generator_charge_count >= 2):
 		if (randi()%2==1):
-			get_tree().change_scene_to_file("res://scenes/Lvl1.tscn")
+			get_tree().change_scene_to_file("res://scenes/Levels/Lvl1.tscn")
 		else:
-			get_tree().change_scene_to_file("res://scenes/Lvl2.tscn")
+			get_tree().change_scene_to_file("res://scenes/Levels/Lvl2.tscn")
 	return
--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -16,15 +16,15 @@ extends Node
 
 var random = RandomNumberGenerator.new()
 
-var moohs: Array[AudioStreamPlayer] = [mooh_1_sfx, mooh_2_sfx, mooh_3_sfx]
-var catches: Array[AudioStreamPlayer] = [catch_1_sfx, catch_2_sfx]
-var catche_attempts: Array[AudioStreamPlayer] = [catch_attempt_1_sfx, catch_attempt_2_sfx]
+@onready var moohs: Array[AudioStreamPlayer] = [mooh_1_sfx, mooh_2_sfx, mooh_3_sfx]
+@onready var catches: Array[AudioStreamPlayer] = [catch_1_sfx, catch_2_sfx]
+@onready var catche_attempts: Array[AudioStreamPlayer] = [catch_attempt_1_sfx, catch_attempt_2_sfx]
 
 func get_rand_mooh() -> AudioStreamPlayer:
-	return moohs[random.randi_range(0, moohs.size())]
+	return moohs[random.randi_range(0, moohs.size()-1)]
 
 func get_rand_catch() -> AudioStreamPlayer:
-	return catches[random.randi_range(0, catches.size())]
+	return catches[random.randi_range(0, catches.size()-1)]
 	
 func get_rand_catch_attempt() -> AudioStreamPlayer:
-	return catche_attempts[random.randi_range(0, catche_attempts.size())]
+	return catche_attempts[random.randi_range(0, catche_attempts.size()-1)]
