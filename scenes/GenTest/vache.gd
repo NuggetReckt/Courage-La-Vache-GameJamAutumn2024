6c4c4ed47709429951063b9196bd1f65a33eb587
Mecagon <justin.lepinepro@protonmail.com>
2024-11-16 13:35:22 +0100

    MEUUHHHHHH

--- scenes/GenTest/vache.gd
+++ scenes/GenTest/vache.gd
@@ -7,7 +7,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (randi()%3000==1):
+	if (randi()%5000==1):
 		find_child("CPUParticles2D").emitting = true
 		await get_tree().create_timer(1.5).timeout
 		find_child("CPUParticles2D").emitting = false
