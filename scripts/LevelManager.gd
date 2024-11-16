7e6345d10964a91ef46827587fe03484ef112eaf
herbyvor <50072047+jeanfrancoisemil@users.noreply.github.com>
2024-11-16 11:54:18 +0100

    level manager & tuto fixed

--- scripts/LevelManager.gd
+++ scripts/LevelManager.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 
+var phase : int #0 = jour / 1 = nuit (lmao cheh corto g la flm de faire une enum)
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,4 +14,8 @@ func _process(delta: float) -> void:
 	if (Main.generator_charge_count >= 3):
 		print("les vaches ont gagné !")
 		get_tree().change_scene_to_file("res://scenes/CowWin.tscn")
+	if (Main.players_alive_count <= 0):
+		print("le chasseur à gagné !")
+		get_tree().change_scene_to_file("res://scenes/HunterWin.tscn")
+
 	return
