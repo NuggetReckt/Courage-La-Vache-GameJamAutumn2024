3d5d2a9cf87b91387401bae4dca375df8cfe5578
NuggetReckt <corto.morrow@gmail.com>
2024-11-16 12:00:07 +0100

    feat: player alive counter

--- scripts/LevelManager.gd
+++ scripts/LevelManager.gd
@@ -1,13 +1,12 @@
 extends Node2D
 
+@onready var player_counter: Label = $PlayerCounter
 
 var phase : int #0 = jour / 1 = nuit (lmao cheh corto g la flm de faire une enum)
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -17,5 +16,5 @@ func _process(delta: float) -> void:
 	if (Main.players_alive_count <= 0):
 		print("le chasseur à gagné !")
 		get_tree().change_scene_to_file("res://scenes/HunterWin.tscn")
-
-	return
+	
+	player_counter.text = str(Main.players_alive_count)
